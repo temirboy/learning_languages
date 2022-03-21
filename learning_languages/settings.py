c980d4f4b77d4205ea53f87aadf3fd78270a247c
temirboy <nvn2030nvn@gmail.com>
2022-03-21 08:03:21 +0600

    Add login users

--- learning_languages/settings.py
+++ learning_languages/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'learning_languages.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- learning_languages/urls.py
+++ learning_languages/urls.py
@@ -20,5 +20,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('', TemplateView.as_view(template_name='users/home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
